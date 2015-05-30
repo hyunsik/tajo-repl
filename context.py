a9625281f2cd4b5b4cd022eed8359a1d486c9563
Hyunsik Choi <hyunsik@apache.org>
2015-05-30 02:49:46 -0700

    Add some methods context.py and update generated pys.

--- context.py
+++ context.py
@@ -1,30 +1,21 @@
-from proto import pkernel_client_protocol_pb2
 from proto import TajoIdProtos_pb2
 from proto import PrimitiveProtos_pb2
+from proto import TajoMasterClientProtocol_pb2 as client
+from proto import ClientProtos_pb2
 
 class TajoContext:
     host = ""
     port = 0
 
-    def __init__(self, host="localhost", port="26080"):
+    def __init__(self, host="localhost", port=28002):
         self.host = host
         self.port = port
 
-    # def stop(self):
-    #
-    # def textFile(self, path):
-    #
-    # def table(self, path, format):
-    #
-    # def broadcast(self, value):
-    #
-    # def addFile(self, path):
-    #
-    # def addPyFile(self, path):
+    def createSession(self):
+        _TIMEOUT_SECONDS = 10
+        with client.early_adopter_create_TajoMasterClientProtocolService_stub(self.host, self.port) as stub:
+            session = stub.CreateSession(ClientProtos_pb2.CreateSessionRequest(username="hyunsik"), _TIMEOUT_SECONDS)
 
-    def username(self):
-        session_id = TajoIdProtos_pb2.SessionIdProto;
-        session_id.id = "x1234567890"
-        with pkernel_client_protocol_pb2.early_adopter_create_PKernelClientProtocol_stub('localhost', 20065) as stub:
-            response = stub.GetUsername(session_id)
-            print "Greeter client received: " + response.value
+
+c = TajoContext()
+c.createSession()
